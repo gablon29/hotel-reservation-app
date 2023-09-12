e726a332394b9a73332124f1824fa9b50d16b316
gablon29 <gabriellondero40@gmail.com>
2023-09-12 11:11:53 -0300

    terminando el componente de BookingForm y renderizando en el componente HotelDetail

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,20 @@ import { Route, Switch } from "wouter";
 import HotelList from "./components/HotelList";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import HotelDetail from "./components/HotelDetail";
+import { Toaster } from "react-hot-toast";
 
 const client = new QueryClient();
 function App() {
   return (
-    <QueryClientProvider client={client}>
-      <Switch>
-        <Route path="/" component={HotelList} />
-        <Route path="/hotel/:id" component={HotelDetail} />
-      </Switch>
-    </QueryClientProvider>
+    <>
+      <Toaster position="top-left" reverseOrder={false} />
+      <QueryClientProvider client={client}>
+        <Switch>
+          <Route path="/" component={HotelList} />
+          <Route path="/hotel/:id" component={HotelDetail} />
+        </Switch>
+      </QueryClientProvider>
+    </>
   );
 }
 
--- src/components/BookingForm.jsx
+++ src/components/BookingForm.jsx
@@ -1,7 +1,38 @@
 import React from "react";
+import { useForm } from "react-hook-form";
+import { Typography, Input, Button } from "@mui/material";
+import toast from "react-hot-toast";
+import useStore from "../store";
 
-const BookingForm = () => {
-  return <div>BookingForm</div>;
+const BookingForm = ({ hotel }) => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  const addReservation = useStore((state) => state.addReservation);
+
+  const onSubmit = (data) => {
+    addReservation(hotel, data);
+    toast.success("Reservation made!");
+  };
+  return (
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <Input type="date" {...register("startDate", { required: true })} />
+      {errors.startDate && (
+        <Typography style={{ color: "red" }}>Start Date is required</Typography>
+      )}
+      <br />
+      <Input type="date" {...register("endDate", { required: true })} />
+      {errors.endDate && (
+        <Typography style={{ color: "red" }}>End Date is required</Typography>
+      )}
+      <Button variant="contained" type="submit">
+        Make Reservation
+      </Button>
+    </form>
+  );
 };
 
 export default BookingForm;
